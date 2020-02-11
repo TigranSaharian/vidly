6a793f920a816094126cfbc9eb4905832425d763
Tigran Saharyan <tigran1205@gmail.com>
2020-02-11 23:40:16 +0400

    New updates

---
+++ vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using vidly.Dto;
+using vidly.Models;
+
+namespace vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customer, CustomerDto>();
+            CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
---
+++ vidly/Dto/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using vidly.Models;
+
+namespace vidly.Dto
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [Display(Name = "Date of Birth")]
+        [Adult]
+        public DateTime? Birthday { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        [Display(Name = "Membership Type")]
+        public byte MembershipTypeId { get; set; }
+    }
+}
